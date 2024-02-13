0e7c42f5f5a5254f427e9dcf4beb35bd40d5204d
Jahrelm <devjahrel@gmail.com>
2024-02-13 18:27:46 -0500

    Fixing UserService

--- src/main/java/com/example/budgetmanagement/service/UserService.java
+++ src/main/java/com/example/budgetmanagement/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.budgetmanagement.service;
 
 import com.example.budgetmanagement.model.ApplicationUser;
 import com.example.budgetmanagement.model.Role;
+import com.example.budgetmanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,16 +19,13 @@ public class UserService implements UserDetailsService {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         System.out.println("In the user details service");
-        if(!username.equals("Jahrel")) throw new UsernameNotFoundException("User Not Found");
-
-        Set<Role> roles = new HashSet<>();
-        roles.add(new Role(1, "USER"));
-
-        return new ApplicationUser(1, "Jahrel", encoder.encode("password"), roles);
 
         }
 
